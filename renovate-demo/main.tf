b8e51444ff562c498d8a0dde81d2f432f5d044dd
yuta0805 <yuta-hamada@freee.co.jp>
2024-04-15 22:25:08 +0900

    add

---
+++ renovate-demo/main.tf
@@ -0,0 +1,6 @@
+module "renovate_demo" {
+  source = "git@github.com:yuta0805/terraform-aws-hoge?ref=v0.0.0"
+    # source = "git@github.com:C-FO/demo-application-template-module.git?ref=v1.1.0"
+
+  name = "hoge"
+}
---
+++ renovate-demo/provider.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  backend "s3" {
+    bucket = "sre-onboarding-hamada"
+    key    = "onboarding"
+    region = "ap-northeast-1"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  alias  = "virginia"
+  region = "us-east-1"
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
